6afbf971b92c426695d49f472c4ef5957550b806
taroimo0523 <taroimoaki@gmail.com>
2019-06-25 02:22:15 +0900

    Lsson4 発展課題提出
    魔法攻撃用の関数作成および、それによる文字の表示を追加しました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class Boss {
     private int hp = 100;　　　//体力
     private int power = 25;　//攻撃力
+    int mp = 53;   //マジックパワー
 
- public void Attack(){
+    public void Attack() {
         //攻撃用の関数
         Debug.Log(this.power + "のダメージを与えた");
-        }
-public void defence(int damage){
+    }
+    public void defence(int damage) {
         //防御用の関数
-        Debug.Log(damage+"のダメージを受けた");
+        Debug.Log(damage + "のダメージを受けた");
         //残りhpを減らす
         this.hp -= damage;
+    }
+    public void Magic(int times) {
+        //魔法攻撃用の関数、timesは魔法攻撃の試行回数
+       
+        if (mp >= 5)
+        {
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りのMPは" + mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない");
+        }
     }
 }
 
@@ -22,13 +36,11 @@ public class Test : MonoBehaviour {
 
     
     void Start() {
-        //要素数5の配列arrayを初期化する
-        int[] array = { 20, 40, 50, 10, 200 };
-        //要素数分の処理を繰り返す
-        for (int i=4;i>=0;i--){
-            //コンソールに表示する
-            Debug.Log(array[i]);
-
+        Boss lastboss = new Boss();
+        //10回魔法を使って正しく表示されるか、確認
+        for (int i = 0; i <= 10; i++)
+        {
+            lastboss.Magic(i);
         }
     }
 	// Update is called once per frame
